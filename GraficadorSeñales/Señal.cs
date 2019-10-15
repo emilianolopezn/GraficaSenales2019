1953b52fd2ae68f470cedf7262b2d265103a3c3f
Emiliano Lopez <emilianolopezn@outlook.com>
2019-10-15 11:32:21 -0700

    transformada

--- GraficadorSeñales/Señal.cs
+++ GraficadorSeñales/Señal.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
+
 
 namespace GraficadorSeñales
 {
@@ -69,6 +71,35 @@ namespace GraficadorSeñales
                     resultado.AmplitudMaxima =
                         Math.Abs(nuevoValor);
                 }
+            }
+            return resultado;
+        }
+
+        public static Señal transformadaFourier(Señal señal)
+        {
+            SeñalResultante resultado =
+                new SeñalResultante();
+
+            resultado.TiempoInicial = señal.TiempoInicial;
+            resultado.TiempoFinal = señal.TiempoFinal;
+            resultado.FrecuenciaMuestreo = señal.FrecuenciaMuestreo;
+
+            for (int k=0; k < señal.Muestras.Count; k++)
+            {
+             
+                Complex muestra = 0; // 0 + 0i
+                for (int n=0; n< señal.Muestras.Count; n++)
+                {
+                    muestra +=
+                        señal.Muestras[n].Y *
+                            Complex.Exp((-2 * Math.PI *
+                            Complex.ImaginaryOne * k * n) /
+                            señal.Muestras.Count);
+                }
+                resultado.Muestras.Add(new Muestra(
+                    señal.Muestras[k].X,
+                    muestra.Magnitude
+                    ));
             }
             return resultado;
         }
